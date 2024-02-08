0315ead7e838ae61127587ac26172605317d9718
Matheus Lopes <m.lopeslemos96@gmail.com>
2024-02-08 10:25:21 -0300

    new file:   elenco/elenco.css 	new file:   elenco/elenco.html 	new file:   elenco/elencoResponsive.css 	new file:   img/Elenco_img/ALEXSANDER.png 	new file:   img/Elenco_img/ANDRE.png 	new file:   img/Elenco_img/DIOGO-BARBOSA.png 	new file:   img/Elenco_img/FABIO.png 	new file:   img/Elenco_img/FELIPE-MELO.png 	new file:   img/Elenco_img/GERMAN-CANO.png 	new file:   img/Elenco_img/GUGA.png 	new file:   img/Elenco_img/JHON-ARIAS.png 	new file:   img/Elenco_img/JOHN-KENNEDY.png 	new file:   img/Elenco_img/KENO.png 	new file:   img/Elenco_img/LIMA.png 	new file:   img/Elenco_img/MARCELO.png 	new file:   img/Elenco_img/MARTINELLI.png 	new file:   img/Elenco_img/NINO.png 	new file:   img/Elenco_img/PH-GANSO.png 	new file:   img/Elenco_img/SAMUEL-XAVIER.png 	new file:   "img/Hist\303\263ria.png" 	new file:   img/Noticias_img/Noticia_1.webp 	new file:   img/Noticias_img/Noticia_2.jpg 	new file:   img/Noticias_img/Noticia_3.jpg 	new file:   img/Noticias_img/Noticia_4.jpg 	new file:   img/Noticias_img/Noticia_5.jpg 	new file:   img/Noticias_img/Noticia_6.jpg 	new file:   img/Noticias_img/Noticia_7.jpg 	new file:   img/Noticias_img/Noticia_8.jpg 	new file:   img/Noticias_img/Noticia_9.jpg 	new file:   "img/Slides_img/Not\303\255cias.jpg" 	new file:   "img/Slides_img/Sala de trof\303\251us.jpg" 	new file:   "img/Slides_img/Seja s\303\263cio.jpeg" 	new file:   img/Trofeus_img/Campeonato_carioca.png 	new file:   "img/Trofeus_img/Ta\303\247a-guanabara.png" 	new file:   "img/Trofeus_img/Ta\303\247a_Libertadores_2013.png" 	new file:   "img/Trofeus_img/Ta\303\247a_brasileirao.png" 	new file:   "img/Trofeus_img/Ta\303\247a_copa_do_Brasil.png" 	new file:   "img/Trofeus_img/Ta\303\247a_rio.png" 	new file:   "img/Trofeus_img/Ta\303\247a_rio_internacional.png" 	new file:   img/Trofeus_img/Torneio_de_Paris_1976.png 	new file:   img/Trofeus_img/primeira_liga.png 	new file:   img/fluminense-logo-escudo.png 	new file:   index.html 	new file:   noticias/noticias.css 	new file:   noticias/noticias.html 	new file:   noticias/noticiasResponsive.css 	new file:   responsive.css 	new file:   script.js 	new file:   style.css 	new file:   trofeus/trofeus.css 	new file:   trofeus/trofeus.html 	new file:   trofeus/trofeus.js 	new file:   trofeus/trofeusResponsive.css

---
+++ trofeus/trofeus.js
@@ -0,0 +1,114 @@
+let imgCard1 = document.querySelector('#card1 img')
+let h1Card1 = document.querySelector('#card1 h1')
+let h1Card2 = document.querySelector('#card2 h1')
+let h1Card3 = document.querySelector('#card3 h1')
+let h1Card4 = document.querySelector('#card4 h1')
+let h1Card5 = document.querySelector('#card5 h1')
+let h1Card6 = document.querySelector('#card6 h1')
+let h1Card7 = document.querySelector('#card7 h1')
+let h1Card8 = document.querySelector('#card8 h1')
+let h1Card9 = document.querySelector('#card9 h1')
+let h1Card0 = document.querySelector('#card10 h1')
+let card1 = document.querySelector('#card1')
+let card2 = document.querySelector('#card2')
+let card3 = document.querySelector('#card3')
+let card4 = document.querySelector('#card4')
+let card5 = document.querySelector('#card5')
+let card6 = document.querySelector('#card6')
+let card7 = document.querySelector('#card7')
+let card8 = document.querySelector('#card8')
+let card9 = document.querySelector('#card9')
+let card10 = document.querySelector('#card10')
+
+function changeCard2() {
+
+  imgCard1.src = '../img/Trofeus_img/Taça_Libertadores_2013.png'
+  h1Card1.innerHTML = 'Copa Libertadores da América'
+
+}
+
+card2.addEventListener('click', changeCard2)
+h1Card2.addEventListener('click', changeCard2)
+
+function changeCard3() {
+
+  imgCard1.src = '../img/Trofeus_img/Taça_rio_internacional.png'
+  h1Card1.innerHTML = 'Copa Rio Internacional'
+
+}
+
+card3.addEventListener('click', changeCard3)
+h1Card3.addEventListener('click', changeCard3)
+
+function changeCard4() {
+
+  imgCard1.src = '../img/Trofeus_img/Torneio_de_Paris_1976.png'
+  h1Card1.innerHTML = 'Torneio de Paris'
+
+}
+
+card4.addEventListener('click', changeCard4)
+h1Card4.addEventListener('click', changeCard4)
+
+
+function changeCard5() {
+
+  imgCard1.src = '../img/Trofeus_img/Taça_brasileirao.png'
+  h1Card1.innerHTML = 'Campeonato Brasileiro'
+
+}
+
+card5.addEventListener('click', changeCard5)
+h1Card5.addEventListener('click', changeCard5)
+
+function changeCard6() {
+
+  imgCard1.src = '../img/Trofeus_img/Taça_copa_do_Brasil.png'
+  h1Card1.innerHTML = 'Copa do Brasil'
+
+}
+
+card6.addEventListener('click', changeCard6)
+h1Card6.addEventListener('click', changeCard6)
+
+function changeCard7() {
+
+  imgCard1.src = '../img/Trofeus_img/primeira_liga.png'
+  h1Card1.innerHTML = 'Primeira Liga'
+
+}
+
+card7.addEventListener('click', changeCard7)
+h1Card7.addEventListener('click', changeCard7)
+
+function changeCard8() {
+
+  imgCard1.src = '../img/Trofeus_img/Campeonato_carioca.png'
+  h1Card1.innerHTML = 'Campeonato Carioca'
+
+}
+
+card8.addEventListener('click', changeCard8)
+h1Card8.addEventListener('click', changeCard8)
+
+
+function changeCard9() {
+
+  imgCard1.src = '../img/Trofeus_img/Taça-guanabara.png'
+  h1Card1.innerHTML = 'Taça Guanabara'
+
+}
+
+card9.addEventListener('click', changeCard9)
+h1Card9.addEventListener('click', changeCard9)
+
+
+function changeCard10() {
+
+  imgCard1.src = '../img/Trofeus_img/Taça_rio.png'
+  h1Card1.innerHTML = 'Taça Rio'
+
+}
+
+card10.addEventListener('click', changeCard10)
+h1Card10.addEventListener('click', changeCard10)
